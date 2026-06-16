fb7c69ca3bf24f5f0d0ad454479f249e2f832547
dingus <serverdingus@gmail.com>
2026-06-16 21:25:31 +0100

    Add files via upload

--- src/main/java/com/dungeonrift/listener/InstanceLifecycleListener.java
+++ src/main/java/com/dungeonrift/listener/InstanceLifecycleListener.java
@@ -2,10 +2,12 @@ package com.dungeonrift.listener;
 
 import com.dungeonrift.DungeonRift;
 import com.dungeonrift.model.DungeonInstance;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.weather.LightningStrikeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
